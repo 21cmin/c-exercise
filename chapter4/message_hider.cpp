4c747b3b5d8231c77d1b10805f9bb52576a1dcb2
jeongminlee <chopin.vii@gmail.com>
2022-12-04 21:18:41 +0900

    chpater 4

---
+++ chapter4/message_hider.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "encryptor.h"
+
+int main(int argc, char *argv[])
+{
+  char msg[80];
+  while (fgets(msg, 80, stdin))
+  {
+    encrypt(msg);
+    printf("%s\n", msg);
+  }
+  
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main()
+{
+  char a = 149;
+  char b = 37;
+  char c = 213;
+  char d = 92;
+
+  printf("%c %c %c %c\n", a, b, c, d);
+}
