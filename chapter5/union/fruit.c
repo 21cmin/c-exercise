8d54a86226734e4a1b5ed98f0bdec66f181b4048
min <jeongmin.lee@astems.co.kr>
2022-12-05 12:50:12 +0900

    chapter6

--- chapter5/union/fruit.c
+++ chapter5/union/fruit.c
@@ -1,3 +1,8 @@
+typedef enum 
+{
+  COUNT, POUNDS, PINTS
+} unit_of_measure;
+
 typedef union
 {
   short count;
@@ -10,12 +15,31 @@ typedef struct
   const char *name;
   const char *country;
   quantity amount;
+  unit_of_measure units;
 } fruit_order;
+
+void display(fruit_order order)
+{
+  printf("This order contains ");
+  if (order.units == PINTS)
+    printf("%2.2f pints of %s\n", order.amount.volume, order.name);
+  else if (order.units ==  POUNDS)
+    printf("%2.2f lbs of %s\n", order.amount.weight, order.name);
+  else 
+    printf("%i %s\n", order.amount.count, order.name);
+}
 
 
 int main(int argc, char *argv[]) {
   fruit_order apples = {"apples", "England"};
   apples.amount.weight=4.2;
+  apples.units = COUNT;
 
-  printf("This order contains %2.2f lbs of %s\n", apples.amount.weight, apples.name);
+
+  fruit_order oj = {"orange juice", "U.S.A"};
+  oj.amount.weight = 10.5;
+  oj.units = POUNDS;
+
+  display(apples);
+  display(oj);
 }
